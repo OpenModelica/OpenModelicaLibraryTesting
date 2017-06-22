9a2d89d41e9b220dd956a58dd1bf433ccff7805d
Martin Sjölund <martin.sjolund@liu.se>
2017-06-22 18:14:35 +0200

    Cleanup more tmp-files

--- test.py
+++ test.py
@@ -53,7 +53,7 @@ def runCommand(cmd, prefix, timeout):
     lines = open("%s.tmpfiles" % prefix).readlines()
   except:
     lines = []
-  for suffix in ["_*.o","_*.so","_*.h","_*.c","_*.cpp",".mos","",".c",".cpp","_info.json","_*.xml","_*.tmpfiles","_res.*"]:
+  for suffix in ["_*.o","_*.so","_*.h","_*.c","_*.cpp",".mos","",".o",".h",".c",".cpp","_info.json","_*.xml","_*.tmpfiles","_res.*",".pipe",".tmpfiles",".libs",".log"]:
     for f in glob.glob(prefix+suffix):
       lines.append(f)
     for f in glob.glob("OM"+prefix+suffix):
