d9f6de3f9ab57db23ea2f87dd819885f2011676c
Adrian Pop <adrian.pop@liu.se>
2018-12-03 13:38:36 +0100

    print an message if result file was not generated

--- shared.py
+++ shared.py
@@ -69,6 +69,8 @@ def simulationAcceptsFlag(f, checkOutput=True, cwd=None):
     pass
   try:
     subprocess.check_output("./HelloWorld %s" % f, shell=True, stderr=subprocess.STDOUT, cwd=cwd)
+    if (not os.path.exists("HelloWorld_res.mat")):
+	  print("Result file HelloWorld_res.mat WAS NOT generated running: ./HelloWorld with flags [%s]" % f)
     if (not checkOutput) or os.path.exists("HelloWorld_res.mat"):
       return True
   except subprocess.CalledProcessError as e:
