OpenModelica/OpenModelicaLibraryTesting

commit 74f1379eb4757fddf471f0e95a2bdb77145966d9

Fix html (#51)
* Improve test.py doc in README

* Fixing HTML issues

  - `<html>` missing `DOCTYPE`.
  - Replace `<hr />` with `<hr>`.
  - Replace `<br />` with `<br>`
  - Element p not allowed as child of element span
  - Fix missing </tr>

testmodel.py
@@ -529,12 +529,12 @@ else:
   # Create a file containing only the calibrated variables, for easy display
   lstfiles = "\n".join(['<li>%s <a href="%s.html">(javascript)</a> <a href="%s.csv">(csv)</a></li>' % (str.split(str(f),".diff.",1)[1],str(os.path.basename(f)),str(os.path.basename(f))) for f in diffFiles])
   with open(prefix+".html", 'w') as fp:
-    fp.write("<html><body><h1>%s differences from the reference file</h1><p>startTime: %g</p><p>stopTime: %g</p><p>Simulated using tolerance: %g</p><ul>%s</ul></body></html>" % (conf["modelName"], startTime, stopTime, tolerance, lstfiles));
+    fp.write("<html lang="en"><body><h1>%s differences from the reference file</h1><p>startTime: %g</p><p>stopTime: %g</p><p>Simulated using tolerance: %g</p><ul>%s</ul></body></html>" % (conf["modelName"], startTime, stopTime, tolerance, lstfiles));
   for var in diffVars:
     if "/" in var:
       continue # Quoted identifier, or possibly an error message... Either way, avoid crapping out below
     with open(prefix+"."+var+".html", 'w') as fp:
-      fp.write("""<html>
+      fp.write("""<html lang="en">
 <head>
 <script type="text/javascript" src="dygraph-combined.js"></script>
     <style type="text/css">

.gitignore
@@ -1,3 +1,11 @@
+__pycache__
+!HelloWorld.mo
+!HelloWorld.mos
 /*.json
 /converted-libraries
-__pycache__
+files/
+HelloWorld*
+history/
+sqlite3.db
+/*.html
+/*.files

all-reports.py
@@ -256,7 +256,7 @@ for email in sorted(emails_to_send.keys()):
 The following reports contain regressions your account was involved with:
 """ + "\n".join(reversed(emails_to_send[email]["plain"])))
   msg.add_alternative("""\
-<html>
+<html lang="en">
 <head></head>
 <body>
 <p>The following reports contain regressions your account was involved with:</p>

history.html.tpl
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
   <meta charset="utf-8">
   <title>OpenModelica #BRANCH# from #DATE1# to #DATE2#</title>

library.html.tpl
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
   <title>#fileName# test using OpenModelica</title>
 </head>

overview.html.tpl
@@ -1,4 +1,5 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
   <title>#title#</title>
   <style>

report.py
@@ -19,7 +19,7 @@ branches = [br.split("/")[-1] for br in args.branches.split(" ")]
 dates = {}
 dates_str = {}
 fields = ["exectime", "parsing", "frontend", "backend", "simcode", "templates", "compile", "simulate", "verify"]
-entryhead = "<tr><th>Branch</th><th>Total</th><th>Parsing</th><th>Frontend</th><th>Backend</th><th>SimCode</th><th>Templates</th><th>Compilation</th><th>Simulation</th><th>Verification</th>\n"
+entryhead = "<tr><th>Branch</th><th>Total</th><th>Parsing</th><th>Frontend</th><th>Backend</th><th>SimCode</th><th>Templates</th><th>Compilation</th><th>Simulation</th><th>Verification</th></tr>\n"
 
 libs = {}
 
@@ -70,7 +70,7 @@ def checkEqual(iterator):
 
 for lib in sorted(libs.keys()):
   models = libs[lib]
-  entries += "<hr /><h3>%s</h3>\n" % lib
+  entries += "<hr><h3>%s</h3>\n" % lib
   branches_versions = [(cursor.execute("SELECT libversion FROM [libversion] WHERE libname=? AND branch=? ORDER BY date DESC LIMIT 1", (lib,branch)).fetchone() or ["unknown"])[0] for branch in branches]
   all_equal = checkEqual(branches_versions)
   if not all_equal:
@@ -104,11 +104,11 @@ for lib in sorted(libs.keys()):
       diff2 = models[branches[-1]][i] - models[branch][i]
       diff_text = ""
       if len(diff2)>0:
-        diff_text += "<p>Now working in %s, failed in %s:<br />\n" % (branches[-1],branch) + "<br />\n".join(sorted(diff2)) + "</p>"
+        diff_text += "<p>Now working in %s, failed in %s:<br>\n" % (branches[-1],branch) + "<br>\n".join(sorted(diff2)) + "</p>"
       if len(diff1)>0:
-        diff_text += "<p>Now failing in %s, worked in %s:<br />\n" % (branches[-1],branch) + "<br />\n".join(sorted(diff1)) + "</p>"
+        diff_text += "<p>Now failing in %s, worked in %s:<br>\n" % (branches[-1],branch) + "<br>\n".join(sorted(diff1)) + "</p>"
       if False and diff_text:
-        diff_text += "<p>Debug:<br />\n" + "<br />\n".join(models[branch][i]) + "</p>"
+        diff_text += "<p>Debug:<br>\n" + "<br>\n".join(models[branch][i]) + "</p>"
       entries += '<td%s><a%s>%s%s</a></td>' % (' class="warning"' if old_vs and old_vs[i]>vs[i] else (' class="better"' if old_vs and old_vs[i]<vs[i] else ""),' class="dot"' if diff_text else "",vs[i],('<span class="tooltip">%s</span>' % diff_text) if diff_text else "")
     entries += "</tr>"
     nsimulate[branch] += vs[6]

commit cecfca83f76d4f95ba5692f544d5b8a0bfb1d8b7

Fixing Python string
  - Error was introduced by 74f1379

testmodel.py
@@ -529,7 +529,7 @@ else:
   # Create a file containing only the calibrated variables, for easy display
   lstfiles = "\n".join(['<li>%s <a href="%s.html">(javascript)</a> <a href="%s.csv">(csv)</a></li>' % (str.split(str(f),".diff.",1)[1],str(os.path.basename(f)),str(os.path.basename(f))) for f in diffFiles])
   with open(prefix+".html", 'w') as fp:
-    fp.write("<html lang="en"><body><h1>%s differences from the reference file</h1><p>startTime: %g</p><p>stopTime: %g</p><p>Simulated using tolerance: %g</p><ul>%s</ul></body></html>" % (conf["modelName"], startTime, stopTime, tolerance, lstfiles));
+    fp.write('<html lang="en"><body><h1>%s differences from the reference file</h1><p>startTime: %g</p><p>stopTime: %g</p><p>Simulated using tolerance: %g</p><ul>%s</ul></body></html>' % (conf["modelName"], startTime, stopTime, tolerance, lstfiles))
   for var in diffVars:
     if "/" in var:
       continue # Quoted identifier, or possibly an error message... Either way, avoid crapping out below