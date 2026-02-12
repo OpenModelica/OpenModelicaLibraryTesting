6241a6132eec0fe4a991504bc8dab2ae0ad93dd9
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2026-02-12 14:54:24 +0100

    Updating TestBaseModelica Manifest

--- basemodelica.py
+++ basemodelica.py
@@ -21,7 +21,9 @@ def print_julia_version() -> None:
   julia = shutil.which("julia")
   print("Julia executable: %s" %(julia))
 
-  jl.seval("using InteractiveUtils; versioninfo()")
+  # Jenkins might redirect STDOUT and Julia prints aren't displayed
+  versioninfo = jl.seval("using InteractiveUtils; buf = IOBuffer(); versioninfo(buf); String(take!(buf))")
+  print(versioninfo)
 
 def precompile_testbaesmodelica(systemImage: os.PathLike | None = None) -> None:
   """Update and pre-compile TestBaseModelica to `sysimage`.
